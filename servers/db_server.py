00257df96037332d3eeec9aecbafe4ba1254f10e
lzy <twolock1997@gmail.com>
2025-04-08 00:05:14 +0800

    排行榜只获取前十条

--- servers/db_server.py
+++ servers/db_server.py
@@ -329,7 +329,7 @@ class DbMsgServer:
         conn, cursor = openDb(messageDb)
         try:
             cursor.execute(
-                "select wxName, count(*) as count from chatMessage where chatMessage.roomId = ? and strftime('%Y-%m-%d', createTime, 'localtime') = strftime('%Y-%m-%d', 'now', 'localtime') group by wxId order by count desc;",
+                "select wxName, count(*) as count from chatMessage where chatMessage.roomId = ? and strftime('%Y-%m-%d', createTime, 'localtime') = strftime('%Y-%m-%d', 'now', 'localtime') group by wxId order by count desc LIMIT 10;",
                 (roomId,))
             result = cursor.fetchall()
             closeDb(conn, cursor)
