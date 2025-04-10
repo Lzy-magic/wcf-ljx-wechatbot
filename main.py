acbdaaaf12f273f4630b3a420d65eb3cf2ab696c
lzy <twolock1997@gmail.com>
2025-04-10 10:53:03 +0800

    在程序退出时输出日志

--- main.py
+++ main.py
@@ -76,5 +76,4 @@ if __name__ == '__main__':
         logger.info("主程序在顶层收到 KeyboardInterrupt，准备退出...")
         # 在这里添加任何需要在程序完全退出前执行的清理代码
         pass  # 或者执行其他清理操作
-    finally:
         logger.info("程序退出。")
